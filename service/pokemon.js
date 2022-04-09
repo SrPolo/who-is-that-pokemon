c5156a61a854091fcf531cfd1754f7ec2686b323
Juan Pablo <juanpablopolancor22@gmail.com>
2022-04-09 10:49:54 -0500

    obtener pokemon hasta la 5ta gen

--- service/pokemon.js
+++ service/pokemon.js
@@ -2,7 +2,7 @@ const POKEMON_URL = "https://pokeapi.co/api/v2/pokemon";
 
 export async function getPokemons() {
   const url = new URL(POKEMON_URL);
-  url.searchParams.append("limit", 150);
+  url.searchParams.append("limit", 721);
   const response = await fetch(url);
   const data = await response.json();
   return data;
