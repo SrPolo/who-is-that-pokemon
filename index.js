a984682e33d90769e4a8d18ca9e77afca2e7544e
Juan Pablo <juanpablopolancor22@gmail.com>
2022-04-09 11:22:26 -0500

    Boton de nuevo juego

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const opciones = document.querySelectorAll(".option");
 const pokemonImageElement = document.getElementById("pokemon-image");
 const pokemonNameElement = document.getElementById("pokemon-name");
 const scoreElement = document.getElementById("score");
+const newGameBTN = document.getElementById("new-game-btn");
 
 let success = new Audio("../assets/success-sound.mp3");
 let wrong = new Audio("../assets/wrong-sound.mp3");
@@ -15,25 +16,33 @@ let pokemonList = [];
 let pokemonOptions = [];
 let pokemonToGuess = {};
 
+function rightAnswer() {
+  success.play();
+  pokemonNameElement.classList.remove("oculto");
+  pokemonImageElement.classList.remove("hide-pokemon");
+  score++;
+  scoreElement.innerText = score;
+  opciones.forEach((button, key) => {
+    button.disabled = true;
+  });
+  setTimeout(() => {
+    newGame();
+  }, 2000);
+}
+
+function wrongAnswer() {
+  wrong.play();
+  pokemonImageElement.classList.add("wrong-answer");
+  setTimeout(() => {
+    pokemonImageElement.classList.remove("wrong-answer");
+  }, 700);
+}
+
 function checkOption(event) {
   if (event.target.value === pokemonName) {
-    success.play();
-    pokemonNameElement.classList.remove("oculto");
-    pokemonImageElement.classList.remove("hide-pokemon");
-    score++;
-    scoreElement.innerText = score;
-    opciones.forEach((button, key) => {
-      button.disabled = true;
-    });
-    setTimeout(() => {
-      newGame();
-    }, 2000);
+    rightAnswer();
   } else {
-    wrong.play();
-    pokemonImageElement.classList.add("wrong-answer");
-    setTimeout(() => {
-      pokemonImageElement.classList.remove("wrong-answer");
-    }, 700);
+    wrongAnswer();
   }
 }
 
@@ -57,10 +66,17 @@ async function newGame() {
   });
 }
 
+function resetGame() {
+  score = 0;
+  scoreElement.innerText = score;
+  newGame();
+}
+
 async function fetchData() {
   const { results } = await getPokemons();
   pokemonList = results;
   newGame();
 }
 
+newGameBTN.addEventListener("click", resetGame);
 fetchData();
