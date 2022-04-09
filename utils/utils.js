a43d2348c3c610d24aac6c1b7b6184498957e7c5
Juan Pablo <juanpablopolancor22@gmail.com>
2022-04-09 11:09:25 -0500

    cambiada funcion para barajar el array

--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,21 @@
 export function getRandomPokemon(array, n) {
-  const shuffled = array.sort(() => 0.5 - Math.random());
+  const shuffled = shuffle([...array]);
   return shuffled.slice(0, n);
+}
+
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
 }
