2e808e9020b5af37f722c283054f592ce6b9368e
Juan Pablo <juanpablopolancor22@gmail.com>
2022-04-09 10:50:39 -0500

    funcionalidad del gameplay terminada

--- index.js
+++ index.js
@@ -6,37 +6,61 @@ const pokemonImageElement = document.getElementById("pokemon-image");
 const pokemonNameElement = document.getElementById("pokemon-name");
 const scoreElement = document.getElementById("score");
 
+let success = new Audio("../assets/success-sound.mp3");
+let wrong = new Audio("../assets/wrong-sound.mp3");
+
 let pokemonName = "";
 let score = 0;
+let pokemonList = [];
+let pokemonOptions = [];
+let pokemonToGuess = {};
 
 function checkOption(event) {
   if (event.target.value === pokemonName) {
+    success.play();
     pokemonNameElement.classList.remove("oculto");
+    pokemonImageElement.classList.remove("hide-pokemon");
     score++;
     scoreElement.innerText = score;
+    opciones.forEach((button, key) => {
+      button.disabled = true;
+    });
+    setTimeout(() => {
+      newGame();
+    }, 2000);
   } else {
-    console.log("Intenta otra vez");
+    wrong.play();
+    pokemonImageElement.classList.add("wrong-answer");
+    setTimeout(() => {
+      pokemonImageElement.classList.remove("wrong-answer");
+    }, 700);
   }
 }
 
-async function setGame(pokemonOptions, pokemonToGuess) {
-  pokemonNameElement.innerText = pokemonToGuess.name;
-  pokemonName = pokemonToGuess.name;
+async function newGame() {
+  pokemonOptions = getRandomPokemon(pokemonList, 4);
+  [pokemonToGuess] = getRandomPokemon(pokemonOptions, 1);
   const pokemon = await getPokemonInfo(pokemonToGuess.url);
+
+  pokemonName = pokemonToGuess.name;
+  pokemonNameElement.classList.add("oculto");
+  pokemonImageElement.classList.add("hide-pokemon");
+  pokemonNameElement.innerText = pokemonName;
   pokemonImageElement.src =
     pokemon.sprites.other["official-artwork"].front_default;
+
   opciones.forEach((button, key) => {
     button.innerHTML = pokemonOptions[key].name;
     button.value = pokemonOptions[key].name;
+    button.disabled = false;
     button.addEventListener("click", checkOption);
   });
 }
 
-async function newGame() {
-  const { results: pokemonList } = await getPokemons();
-  const pokemonOptions = getRandomPokemon(pokemonList, 4);
-  const [pokemonToGuess] = getRandomPokemon(pokemonOptions, 1);
-  setGame(pokemonOptions, pokemonToGuess);
+async function fetchData() {
+  const { results } = await getPokemons();
+  pokemonList = results;
+  newGame();
 }
 
-newGame();
+fetchData();
